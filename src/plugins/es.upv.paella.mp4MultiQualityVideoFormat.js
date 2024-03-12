d62909c44a7c3e9dee9048c7c72f7b7ec215ad87
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2024-03-12 17:19:41 +0100

    Improve quality change logic
    This improves this in two ways:
    
    - When the video is paused before the quality change, the video remains
      paused. Previously it sometimes started playing automatically.
    - The first frame of the video isn't flashed anymore when changing
      quality. This was happening before due to `currentTime` being 0 at
      some point.
    
    Now everything seems to work nicely.

--- src/plugins/es.upv.paella.mp4MultiQualityVideoFormat.js
+++ src/plugins/es.upv.paella.mp4MultiQualityVideoFormat.js
@@ -8,7 +8,8 @@ export class Mp4MultiQualityVideo extends Mp4Video {
                 label: `${src.res.w}x${src.res.h}`,
                 shortLabel: `${src.res.h}p`,
                 width: src.res.w,
-                height: src.res.h
+                height: src.res.h,
+                src: src.src,
             }));
         }
 
@@ -20,16 +21,29 @@ export class Mp4MultiQualityVideo extends Mp4Video {
             throw new Error("Invalid parameter setting video quality");
         }
 
+        this.player.log.debug(`org.opencast.paella.mp4MultiQualityVideoFormat: Change video quality to ${q.shortLabel}`);
         this._currentQuality = q;
 
+        // Clear data, set the `src` attribute to the new video file and then
+        // set some values to previous values.
         const currentTime = this.video.currentTime;
         const playbackRate = this.video.playbackRate;
-
         this.clearStreamData();
-        await this.loadStreamData(this._streamData);
-
+        this.video.src = q.src;
         this.video.currentTime = currentTime;
         this.video.playbackRate = playbackRate;
+        this.video.addEventListener('ended', this._endedCallback);
+
+        // Wait for the `canplay` event to know that the video has loaded sufficiently.
+        await new Promise(resolve => {
+            const f = () => {
+                this._ready = true;
+                this.video.pause();
+                this.video.removeEventListener('canplay', f);
+                resolve(null);
+            };
+            this.video.addEventListener('canplay', f);
+        });
     }
 
     get currentQuality() {
